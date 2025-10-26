503a9db63162b7cbebb56216a6812ddd3ad9f174
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2025-10-25 21:50:40 -0300

    Versao Webapi fase3

---
+++ WebAPI/Controllers/PagamentoControllerHandler.cs
@@ -0,0 +1,35 @@
+﻿using Adapters.Controllers.Interfaces;
+using Adapters.Presenters.QRCode;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class PagamentoControllerHandler : ControllerBase
+    {
+        private readonly ILogger<PagamentoControllerHandler> _logger;
+
+        public PagamentoControllerHandler(ILogger<PagamentoControllerHandler> logger)
+        {
+            _logger = logger;            
+        }
+
+        [HttpPost("WehookNotificacaoPagamento")]
+        [ProducesResponseType(typeof(NotificacaoPagamentoRequest), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> NotificacaoPagamentoAsync([FromBody] NotificacaoPagamentoRequest body, [FromServices] IPagamentoController controller)
+        {
+            Console.WriteLine("Webhook recebido: " + body.ToString());
+
+            if (body is not null)            
+               await controller.ConsultarPagamento(body.Id);            
+
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,109 @@
+using Microsoft.OpenApi.Models;
+using WebAPI.Configurations;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+        });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Lanchonete FIAP-SOAT11",
+        Description = "ASP.NET Core Web API Para o sistema de fastfood da Lanchonete FIAP-SOAT11.",
+        Contact = new OpenApiContact
+        {
+            Name = "Grupo 140 da SOAT11",
+            Url = new Uri("https://github.com/fiap-soat11")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Página do Projeto",
+            Url = new Uri("https://github.com/fiap-soat11"),
+        }
+    });
+
+    // Configuração correta do esquema de segurança JWT no Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT no formato: Bearer {seu token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.WriteIndented = true; // opcional
+});
+
+builder.Services.AddHttpClient();
+builder.Services.AddInfraStructure(builder.Configuration);
+builder.Services.AddValidators(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+var supportedCultures = new[] { "pt-BR", "br" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
